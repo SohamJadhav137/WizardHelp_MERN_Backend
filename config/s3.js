6942d78b379baf351f5a9f1633baa6c8716501c4
SohamJadhav137 <sohamjadhav1503@gmail.com>
2026-01-05 23:41:18 +0530

    Improved login box and started working on signup. Made separate components for login & signup and stored them in AuthPage  parent component. Yet to complete signup step wise functionality.

--- config/s3.js
+++ config/s3.js
@@ -2,7 +2,6 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import { S3Client } from "@aws-sdk/client-s3";
-console.log("AWS_REGION:", process.env.AWS_REGION);
 const s3 = new S3Client({
     region: process.env.AWS_REGION,
     credentials: {
