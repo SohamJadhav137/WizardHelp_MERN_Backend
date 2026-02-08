9f213a8738420c356b062beaf596a61bb587a564
SohamJadhav137 <sohamjadhav1503@gmail.com>
2026-02-09 00:06:58 +0530

    Fixed 'best performing gigs' section in home page where it earlier included unpublished gigs as well but now it considers only published gigs. Also added a guard route in react router to redirect to home page when any irrelevant URL is typed.

--- controllers/gigsController.js
+++ controllers/gigsController.js
@@ -4,20 +4,20 @@ import { deleteAllFilesFromS3 } from "../utils/deleteAllFilesFromS3.js";
 
 export const createGig = async (req, res) => {
     const errors = validationResult(req);
-    console.log("Backend validation:\n",errors);
-    
-    if(!errors.isEmpty())
+    console.log("Backend validation:\n", errors);
+
+    if (!errors.isEmpty())
         return res.status(400).json({ message: errors.array()[0].msg })
-    
+
     try {
-        if (req.user.role !== "seller"){
-            return res.status(403).json({ message: "Only sellers can create gigs!"});
+        if (req.user.role !== "seller") {
+            return res.status(403).json({ message: "Only sellers can create gigs!" });
         }
 
         const { imageURLs } = req.body;
 
         const coverImageURL = imageURLs ? imageURLs[0] : "";
-        
+
         const newGig = new Gig({
             userId: req.user._id,
             coverImageURL: coverImageURL,
@@ -25,92 +25,92 @@ export const createGig = async (req, res) => {
         });
 
         console.log(newGig)
-        
+
         const savedGig = await newGig.save();
 
         res.status(201).json(savedGig);
     } catch (error) {
         console.error("CUSTOM ERROR", error);
-        res.status(500).json({ message: "Faild to create gig!"});
+        res.status(500).json({ message: "Faild to create gig!" });
     }
 };
 
 export const getGigs = async (req, res) => {
-    try{
+    try {
         const gigs = await Gig.find();
         res.status(200).json(gigs);
     } catch (error) {
-        res.status(500).json({ message: "Failed to retreive gigs!"});
+        res.status(500).json({ message: "Failed to retreive gigs!" });
     }
 };
 
 export const getSingleGig = async (req, res) => {
-    try{
+    try {
         const gig = await Gig.findById(req.params.id);
-        if(!gig) return res.status(404).json({ message: "Gig not found!"});
+        if (!gig) return res.status(404).json({ message: "Gig not found!" });
 
         let isVideo = false;
         let mediaURLs = [...gig.imageURLs];
-        if(gig.videoURL){
+        if (gig.videoURL) {
             isVideo = true;
-            mediaURLs = [ gig.videoURL, ...mediaURLs];
+            mediaURLs = [gig.videoURL, ...mediaURLs];
         }
 
         res.status(200).json({ gig, mediaURLs });
     } catch (error) {
         console.error("CUSTOM ERROR:", error);
-        res.status(500).json({ message: "Failed to retreive the gig!"});
+        res.status(500).json({ message: "Failed to retreive the gig!" });
     }
 };
 
 export const getSellerGigs = async (req, res) => {
     const userId = req.user.id;
-    try{
+    try {
         const userGigs = await Gig.find({ userId });
         res.status(200).json(userGigs);
     } catch (error) {
-        console.error("CUSTOM ERROR:",error);
-        res.status(500).json({ message: "Failed to retrieve user specific gigs!"});
+        console.error("CUSTOM ERROR:", error);
+        res.status(500).json({ message: "Failed to retrieve user specific gigs!" });
     }
 }
 
 export const deleteGig = async (req, res) => {
     try {
         const gig = await Gig.findById(req.params.id);
-        if(!gig) return res.status(404).json({ message: "Gig not found!" });
+        if (!gig) return res.status(404).json({ message: "Gig not found!" });
 
         const allFiles = [
             ...(gig.imageURLs || []),
             ...(gig.videoURL || []),
             ...(gig.docURLs || [])
         ];
-        
-        if(req.user._id.toString() !== gig.userId.toString())
-            return res.status(403).json({ message: "Only gig owner can delete the gig!"});
-        
+
+        if (req.user._id.toString() !== gig.userId.toString())
+            return res.status(403).json({ message: "Only gig owner can delete the gig!" });
+
         await deleteAllFilesFromS3(allFiles);
 
         await Gig.findByIdAndDelete(req.params.id);
 
-        res.status(200).json({ message: "Gig and its files deleted successfully"});
+        res.status(200).json({ message: "Gig and its files deleted successfully" });
     } catch (error) {
-        console.error("CUSTOM ERROR:",error);
-        res.status(500).json({ message: "Failed to delete the gig!"});
+        console.error("CUSTOM ERROR:", error);
+        res.status(500).json({ message: "Failed to delete the gig!" });
     }
 };
 
 export const updateGig = async (req, res) => {
     const errors = validationResult(req);
-    console.log("Backend validation:\n",errors);
-    if(!errors.isEmpty())
+    console.log("Backend validation:\n", errors);
+    if (!errors.isEmpty())
         return res.status(400).json({ message: errors.array()[0].msg });
 
     try {
         const gig = await Gig.findById(req.params.id);
-        if(!gig) return res.status(404).json({ message: "Gig not found!"});
+        if (!gig) return res.status(404).json({ message: "Gig not found!" });
 
-        if(req.user._id.toString() !== gig.userId.toString())
-            return res.status(403).json({ message: "Only gig owner can update the gig!"});
+        if (req.user._id.toString() !== gig.userId.toString())
+            return res.status(403).json({ message: "Only gig owner can update the gig!" });
 
         const { imageURLs } = req.body;
 
@@ -127,8 +127,8 @@ export const updateGig = async (req, res) => {
 
         res.status(200).json(updatedGig);
     } catch (error) {
-        console.error("CUSTOM ERROR:",error);
-        res.status(500).json({ message: "Failed to update the gig!"});
+        console.error("CUSTOM ERROR:", error);
+        res.status(500).json({ message: "Failed to update the gig!" });
     }
 };
 
@@ -139,37 +139,44 @@ export const updateGigState = async (req, res) => {
     try {
         const gig = await Gig.findById(gigId);
 
-        if(!gig)
-            return res.status(404).json({ message: "Gig was not found!"});
+        if (!gig)
+            return res.status(404).json({ message: "Gig was not found!" });
 
-        if(req.user._id.toString() !== gig.userId.toString())
-            return res.status(403).json({ message: "Not authorized to change someone's else gig state!"});
+        if (req.user._id.toString() !== gig.userId.toString())
+            return res.status(403).json({ message: "Not authorized to change someone's else gig state!" });
 
         const updatedGig = await Gig.findByIdAndUpdate(
             gigId,
-            { $set: { isPublished: isPublished }},
+            { $set: { isPublished: isPublished } },
             { new: true }
         );
 
         res.status(200).json(updateGig);
     } catch (error) {
-        console.error("CUSTOM ERROR:",error);
-        res.status(500).json({ message: "Some BACKEND error occured while updating gig's state!"});
+        console.error("CUSTOM ERROR:", error);
+        res.status(500).json({ message: "Some BACKEND error occured while updating gig's state!" });
     }
 };
 
 export const getBestGigs = async (req, res) => {
-    try{
+    try {
         const gigs = await Gig.aggregate([
-            { $sort: { orders: -1 } },
-
+            {
+                $match: {
+                    isPublished: true
+                }
+            },
+            {
+                $sort: {
+                    orders: -1
+                }
+            },
             {
                 $group: {
                     _id: "$category",
                     gig: { $first: "$$ROOT" }
                 }
             },
-
             {
                 $project: {
                     _id: 0,
@@ -180,7 +187,7 @@ export const getBestGigs = async (req, res) => {
         ]);
 
         res.status(200).json(gigs);
-    } catch(error){
-        req.status(500).json({ message: "Failed to fetch best gigs" });
+    } catch (error) {
+        res.status(500).json({ message: "Failed to fetch best gigs" });
     }
 };
